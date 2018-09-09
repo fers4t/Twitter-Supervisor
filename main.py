7e0b108a5f338b28ee0b1ef40c39d8bcc7755ac3
Quentin JODER <quentinjoder@gmail.com>
2018-09-09 21:38:06 +0200

    get list of followers ids

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ auth.set_access_token(config.access_token, config.access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print(tweet.text)
 
+followers_ids = api.followers_ids("@quent1gentil")
+followers_number = len(followers_ids)
+print("Nombre de followers: %s" % followers_number)
+print("Premier follower: %s" %followers_ids[0])
 print("---- Fin du programme ----")
