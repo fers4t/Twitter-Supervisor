48c3df32dea553995554053b558a3345185e1713
Quentin JODER <quentinjoder@gmail.com>
2018-09-10 16:10:19 +0200

    Obtain the list of followers and unfollowers (but there is a bug)

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ auth.set_access_token(config.access_token, config.access_token_secret)
 api = tweepy.API(auth)
 
 # GET current followers list
-followers_ids = api.followers_ids(config.username)
-followers_number = len(followers_ids)
+current_followers = set(api.followers_ids(config.username))
+followers_number = len(current_followers)
 print("Current number of followers: %s" %followers_number)
 
 # Persistence configuration
@@ -20,24 +20,34 @@ connection = sqlite3.connect('followers.db')
 c = connection.cursor()
 
 # Get previous followers list
-c.execute('CREATE TABLE IF NOT EXISTS followers (id real)')
+c.execute('CREATE TABLE IF NOT EXISTS followers (id integer)')
 connection.commit()
 c.execute('SELECT * FROM followers')
-previous_followers = c.fetchall()
+previous_followers_list = c.fetchall()
+previous_followers = set()
+for follower in previous_followers_list:
+    previous_followers.add(int(follower[0]))
 print("Previous number of followers: %s" %len(previous_followers))
 
-# TODO: Comparaison des deux listes
-#try:
-#    last_follower = api.get_user(id = followers_ids[0])
-#    name = last_follower.name
-#    print("Dernier follower: ?" ?name)
-#except tweepy.TweepError:
-#    print(TweepError.response.text)
+# Display new followers & unfollowers
+def displayMessageAboutUsers(message, user_ids):
+    for user_id in user_ids:
+        try:
+            user = api.get_user(user_id)
+            print(message %user.name)
+        except tweepy.TweepError:
+            print(TweepError.response.text)
+
+new_followers = current_followers - previous_followers
+displayMessageAboutUsers("%s vous suit désormais.", new_followers)
+unfollowers = previous_followers - current_followers
+displayMessageAboutUsers("%s a cessé de vous suivre.", unfollowers)
 
 # Refresh database content
+# TODO: Solve the bug preventing to save the last follower
 c.execute('DELETE FROM followers')
 def id_generator():
-    for id in followers_ids:
+    for id in current_followers:
         yield (id,)
 c.executemany("INSERT INTO followers(id) VALUES(?)", id_generator())
 connection.commit()
