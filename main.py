f02f5b9e38f6df9877f9034e79d76f8422e82ce7
Quentin JODER <quentinjoder@gmail.com>
2018-10-02 18:42:43 +0200

    Create independent functions to call twitter API.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import logging
-import tweepy
 
-import config
 import database
+import twitter_api
 
 # Logging configuration---------------------------------------------------------
 # logging to file
@@ -13,26 +12,18 @@ logging.basicConfig(level=logging.DEBUG,
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
-# set the output format on the console
 formatter = logging.Formatter('%(levelname)-8s: %(message)s')
 console.setFormatter(formatter)
-# add the handler to the root logger
 logging.getLogger('').addHandler(console)
 
 logging.info('Twitter Supervisor launched!')
 
-# Retrieve the previous followers list------------------------------------------
+# Retrieve the previous followers set-------------------------------------------
 previous_followers = database.getPreviousFollowersSet()
 previous_followers_number = len(previous_followers)
 
-# Query the Twitter API---------------------------------------------------------
-# API configuration
-auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-auth.set_access_token(config.access_token, config.access_token_secret)
-api = tweepy.API(auth)
-
-# GET current followers list
-current_followers = set(api.followers_ids(config.username))
+# Get the current followers set-------------------------------------------------
+current_followers = twitter_api.getFollowersSet()
 followers_number = len(current_followers)
 logging.info("Current number of followers: %d" % followers_number)
 
@@ -43,12 +34,8 @@ if previous_followers_number != 0:
     # Method to display new followers & unfollowers
     def displayMessageAboutUsers(message, user_ids):
         for user_id in user_ids:
-            try:
-                user = api.get_user(user_id)
-                logging.info(message %(user.name, user.screen_name))
-            except tweepy.TweepError as error :
-                logging.error("An error happened when we tried to find user\
-                 %d: %s" % (user_id, error.reason))
+            twitter_api.getUser(user_id)
+            logging.info(message %(user.name, user.screen_name));
 
     new_followers = current_followers - previous_followers
     displayMessageAboutUsers("%s (@%s) follows you now.", new_followers)
---
+++ twitter_api.py
@@ -0,0 +1,17 @@
+import tweepy
+import config
+
+# API configuration
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+api = tweepy.API(auth)
+
+def getFollowersSet():
+    return set(api.followers_ids(config.username));
+
+def getUser(userId):
+    try:
+        return api.get_user(userId)
+    except tweepy.TweepError as error :
+        logging.error("An error happened when we tried to find user\
+         %d: %s" % (user_id, error.reason));
