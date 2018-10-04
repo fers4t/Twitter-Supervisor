8eaff30248fd19e26c2b8b652216624097783329
User of a VPN <vpnuser@vps335158.ovh.net>
2018-10-04 20:31:53 +0200

    Reset log level to debug

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import twitter_api
 
 # Logging configuration---------------------------------------------------------
 # logging to file
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
                     filename='twitter_supervisor.log')
