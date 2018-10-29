9447cc0728da12744263232f7371862da0023b83
Quentin JODER <quentinjoder@gmail.com>
2018-10-29 18:04:54 +0100

    Reformat code

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 import sqlite3
 
-def openConnection():
-  connection = sqlite3.connect('followers.db')
-  return connection, connection.cursor()
+
+def open_connection():
+    connection = sqlite3.connect('followers.db')
+    return connection, connection.cursor()
+
 
 def get_previous_followers_set():
-    connection, cursor = openConnection()
+    connection, cursor = open_connection()
     cursor.execute('CREATE TABLE IF NOT EXISTS followers (id integer)')
     connection.commit()
     cursor.execute('SELECT * FROM followers')
@@ -16,12 +18,14 @@ def get_previous_followers_set():
         previous_followers.add(int(follower[0]))
     return previous_followers
 
+
 def id_generator(followers_set):
     for id in followers_set:
         yield (id,)
 
+
 def update(new_followers, unfollowers):
-    connection, cursor = openConnection()
+    connection, cursor = open_connection()
     cursor.executemany("DELETE FROM followers WHERE id=?", id_generator(unfollowers))
     cursor.executemany("INSERT INTO followers(id) VALUES(?)", id_generator(new_followers))
     connection.commit()
--- twitter_api.py
+++ twitter_api.py
@@ -5,28 +5,31 @@ import config
 
 # API configuration
 api = Api(consumer_key=config.CONSUMER_KEY,
-    consumer_secret=config.CONSUMER_SECRET,
-    access_token_key=config.ACCESS_TOKEN,
-    access_token_secret=config.ACCESS_TOKEN_SECRET)
+          consumer_secret=config.CONSUMER_SECRET,
+          access_token_key=config.ACCESS_TOKEN,
+          access_token_secret=config.ACCESS_TOKEN_SECRET)
+
 
 def get_followers_set():
-	try:
-		return set(api.GetFollowerIDs())
-	except error.TwitterError as e:
-		logging.critical('Unable to retrieve followers id list: {}'.format(e.message))
-		return None
-
-def get_user(userId):
     try:
-        return api.GetUser(userId)
+        return set(api.GetFollowerIDs())
     except error.TwitterError as e:
-        logging.error('An error happened while searching for user n°{0}: {1}'.format(userId, e.message))
+        logging.critical('Unable to retrieve followers id list: {}'.format(e.message))
         return None
+
+
+def get_user(user_id):
+    try:
+        return api.GetUser(user_id)
+    except error.TwitterError as e:
+        logging.error('An error happened while searching for user n°{0}: {1}'.format(user_id, e.message))
+        return None
+
 
 def send_direct_message(text):
     logging.info('Sending direct message: \"{}\"'.format(text))
     try:
-        return api.PostDirectMessage(text, screen_name = config.USERNAME)
+        return api.PostDirectMessage(text, screen_name=config.USERNAME)
     except error.TwitterError as e:
         logging.error('Unable to send direct message: {}'.format(e.message))
         return None
